9391a3ce1871450b6303ac33c76aaeeb1251aeed
Rodevp <rodriguezrodrigor12@gmail.com>
2022-05-16 10:37:09 -0500

    creando add seller

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Login from './components/login'
 import Product from './components/product'
 import Register from './components/register'
 import GetSale from './components/sales/get-sale'
+import AddSeller from './components/seller/add-seller'
 
 function App() {
 
   return (
     <div className='App'>
+      <AddSeller />
     </div>
   )
 }
---
+++ src/components/seller/add-seller.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import uuid from 'uuid-random'
+
+function AddSeller() {
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const submitData = (e) => {
+        e.preventDefault()
+        const data = {
+            id: uuid(),
+            name,
+            email,
+            password,
+            role: 'seller'
+        }
+        console.log(data)
+        fetch('http://localhost:3001/api/v1/admin', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('id')}`
+            },
+            body: JSON.stringify(data)
+        })
+        .then( res => res.json() )
+        .then( data => console.log(data) )
+        .catch( error => console.error(error) )
+
+    }
+
+  return (
+    <div>
+        <form onSubmit={submitData}>
+            <div>
+                <label htmlFor="">Nombre</label>
+                <input type="text" onChange={(e) => { setName(e.target.value) } }/>
+            </div>
+            <div>
+                <label htmlFor="">Email</label>
+                <input type="email" onChange={(e) => { setEmail( e.target.value ) } }/>
+            </div>
+            <div>
+                <label htmlFor="">Contraseña</label>
+                <input type="password" onChange={(e) => { setPassword( e.target.value ) } }/>
+            </div>
+            <input type="submit" value="Crear Vendedor" />
+        </form>
+    </div>
+  )
+}
+
+export default AddSeller
